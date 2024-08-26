205df984c5b1d3451c59b4444a44e37f51e7349a
Galvin <22t1020500@husc.edu.vn>
2024-08-26 14:43:47 +0700

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,18 @@
+function handleScroll() {
+    var elements = document.getElementsByClassName('animation');
+    var windowHeight = window.innerHeight;
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var rect = element.getBoundingClientRect();
+
+        if (rect.top < windowHeight && rect.bottom > 0) {
+            element.classList.add('visible');
+        } 
+        // else {
+        //     element.classList.remove('visible');
+        // }
+    }
+}
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
