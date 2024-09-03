1fa113db7b2f7266cbf0e85434d7f5cc6fb3a09c
Quang Đạt <150123935+LeQuangDatNL@users.noreply.github.com>
2024-09-03 16:12:55 +0700

    Add files via upload

--- js/Toasts.js
+++ js/Toasts.js
@@ -1,3 +1,20 @@
+function ThuNhanVe() {
+  emailjs.send("service_clj6q7w", "template_esvp5tv", { 
+      emailCLB : 'bopdepca123321@gmail.com' , // dien gmail cua m dc
+      email : document.getElementById("email").value ,
+      name: document.getElementById("name").value,
+      khoa: document.getElementById("khoa").value,
+      gioitinh: document.getElementById("gioitinh").value,  
+      diemnmlt: document.getElementById("diemnmlt").value,
+      diemktlt: document.getElementById("diemktlt").value
+    })
+}
+function ThuCamOn(){
+  emailjs.send("service_jjxkuxo","template_34fz9qm",{
+    email : document.getElementById("email").value ,
+    name: document.getElementById("name").value
+    });
+}
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('myForm');
   const mustWriteFields = document.querySelectorAll('.must-write');
@@ -23,7 +40,10 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       else {
         event.preventDefault()
-        Toata.classList.add("show");
+        Toata.classList.add("show")
+        ThuNhanVe();
+        ThuCamOn();
+        form.reset();
       }
   });
 });
